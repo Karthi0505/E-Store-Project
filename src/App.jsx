6eea17e499d199a559d4bd7db625a2a504a384e8
raginimegala <ragini.megala@gmail.com>
2026-05-04 00:00:45 +0530

    Story 1 Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './App.css'
+import TopNav from './Components/TopNav'
+
 
 function App() {
 
   return (
-    <div className="App">
-      <div className='header bg-dark'>
-        <div className='row'>
-          <div className='brand my-1'>
-            <h1> eStore </h1>
+    <div className='App'>
+      <TopNav />
 
-            <div className='inp-container'>
-              <input className='form-control' id='search-input' name='search-input'/>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
