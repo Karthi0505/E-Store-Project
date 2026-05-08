9e89412fec81aad27752297425ab826082a78839
raginimegala <ragini.megala@gmail.com>
2026-05-08 22:40:45 +0530

    resolved review comments

--- src/Store/Slice/ProductSlice/ProductSlice.jsx
+++ src/Store/Slice/ProductSlice/ProductSlice.jsx
@@ -1,48 +1,55 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { Shop1, Shop2, Shop3, Shop4, Shop5, Shop6 } from "../../../assets/Utils/Images";
+import {
+  Shop1,
+  Shop2,
+  Shop3,
+  Shop4,
+  Shop5,
+  Shop6,
+} from "../../../assets/Utils/Images";
 
 const initialState = [
-    {
-      id: 1,
-      pName: "Leather Jacket",
-      pPrice: 45,
-      img: Shop1,
-    },
-    {
-      id: 2,
-      pName: "Watch",
-      pPrice: 50,
-      img: Shop2,
-    },
-    {
-      id: 3,
-      pName: "Sunglass",
-      pPrice: 20,
-      img: Shop3,
-    },
-    {
-      id: 4,
-      pName: "Bag",
-      pPrice: 10,
-      img: Shop4,
-    },
-    {
-      id: 5,
-      pName: "Denim",
-      pPrice: 45,
-      img: Shop5,
-    },
-    {
-      id: 6,
-      pName: "Leather Jacket",
-      pPrice: 100,
-      img: Shop6,
-    },
-  ];
+  {
+    id: 1,
+    pName: "Leather Jacket",
+    pPrice: 45,
+    img: Shop1,
+  },
+  {
+    id: 2,
+    pName: "Watch",
+    pPrice: 50,
+    img: Shop2,
+  },
+  {
+    id: 3,
+    pName: "Sunglass",
+    pPrice: 20,
+    img: Shop3,
+  },
+  {
+    id: 4,
+    pName: "Bag",
+    pPrice: 10,
+    img: Shop4,
+  },
+  {
+    id: 5,
+    pName: "Denim",
+    pPrice: 45,
+    img: Shop5,
+  },
+  {
+    id: 6,
+    pName: "Leather Jacket",
+    pPrice: 100,
+    img: Shop6,
+  },
+];
 
 const productSlice = createSlice({
-    name: "Product",
-    initialState
+  name: "Product",
+  initialState,
 });
 
 export default productSlice;
